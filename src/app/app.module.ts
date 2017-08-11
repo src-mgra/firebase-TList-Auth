65ecec4819e842f50dc72fdbfd4bd09008bd577f
Michael Graßmann <michael.grassmann@dekra.com>
2017-08-11 10:06:37 +0200

    Revert "Initial Commit"
    This reverts commit e5089844774991a0fc8b161345d732560201df65.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyDatePickerModule } from 'mydatepicker';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -16,8 +14,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
-    MyDatePickerModule
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
